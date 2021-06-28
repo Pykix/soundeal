f8a25af20438e77b7bb21da7bee0f65912d43a63
Ludovic Gil <py.gil.ludovic@outlook.com>
2021-06-28 11:00:26 +0200

    [INIT] init project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Soundeal",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Soundeal'),
+      ),
+      body: Center(
+        child: Text('Welcome!'),
+      ),
+    );
+  }
+}
