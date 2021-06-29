8dda3709802ea38dbbe1dfd761aca48bca3de00a
Ludovic Gil <py.gil.ludovic@outlook.com>
2021-06-29 08:58:04 +0200

    [IMPL] articles list file

--- lib/main.dart
+++ lib/main.dart
@@ -38,22 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Soundeal'),
-      ),
-      body: Center(
-        child: Text('Welcome!'),
-      ),
-    );
-  }
-}
