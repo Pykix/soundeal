d3fc4f497f89881624118635e2864e030ebb2926
Ludovic Gil <py.gil.ludovic@outlook.com>
2021-06-29 11:14:07 +0200

    [IMPL] add list of fake articles to test

--- lib/models/articles.dart
+++ lib/models/articles.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/material.dart';
-
 class Articles {
   int id;
   String title;
   String desc;
   String state;
+  int price;
+  int age;
+  String type;
+  String user;
 
   Articles(
     this.id,
     this.title,
     this.desc,
     this.state,
+    this.price,
+    this.age,
+    this.type,
+    this.user,
   );
 }
--- lib/widgets/articles_list.dart
+++ lib/widgets/articles_list.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../fake_articles.dart';
+
 class ArticlesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Card(
+          elevation: 5,
+          margin: EdgeInsets.symmetric(
+            horizontal: 5,
+            vertical: 8,
+          ),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: FittedBox(
+                  child: Text('${articles[index].price}€'),
+                ),
+              ),
+            ),
+            title: Text(
+              articles[index].title,
+            ),
+            subtitle: Text(articles[index].desc),
+          ),
+        );
+      },
+      itemCount: articles.length,
+    );
   }
 }
