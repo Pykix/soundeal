39c3f7ec8e5f372c166b9c29cab30e416cd866c7
Pikyx <py.gil.ludovic@outlook.com>
2021-07-04 21:17:51 +0200

    [IMPL] add connection with API for list articles

--- lib/widgets/articles/articles_list.dart
+++ lib/widgets/articles/articles_list.dart
@@ -5,11 +5,15 @@ import 'package:soundeal/models/articles.dart';
 import 'package:http/http.dart' as http;
 import '../../fake_articles.dart';
 
-Future<Articles> fetchArticles() async {
-  final response = await http.get(Uri.parse('http://10.0.2.2:8000/item/29'));
+List<Articles> articlesModelFromJson(String str) =>
+    List<Articles>.from(json.decode(str).map((x) => Articles.fromJson(x)));
+
+Future<List<Articles>> fetchArticles() async {
+  var response =
+      await http.get(Uri.parse('http://10.0.2.2:8000/item/category/21'));
 
   if (response.statusCode == 200) {
-    return Articles.fromJson(jsonDecode(response.body));
+    return articlesModelFromJson(response.body);
   } else {
     throw Exception('Failed to load articles');
   }
@@ -21,28 +25,82 @@ class ArticlesList extends StatefulWidget {
 }
 
 class _ArticlesListState extends State<ArticlesList> {
-  Future<Articles> articles;
+  Future<List<Articles>> articlesObject;
 
   @override
   void initState() {
     super.initState();
-    articles = fetchArticles();
+    articlesObject = fetchArticles();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FutureBuilder<Articles>(
+      child: FutureBuilder(
+        future: articlesObject,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print(snapshot.data.description);
-            return Text(snapshot.data.title);
-          } else if (snapshot.hasError) {
+          if (snapshot.connectionState == ConnectionState.none) {
             return Text("${snapshot.error}");
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  var article = snapshot.data[index];
+
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/detail',
+                        arguments: Articles(
+                          id: article.id,
+                          title: article.title,
+                          description: article.description,
+                          state: article.state,
+                          price: article.price,
+                          age: article.age,
+                          type: article.type,
+                          user: article.user,
+                        ),
+                      );
+                    },
+                    child: Card(
+                      elevation: 3,
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 3,
+                        vertical: 8,
+                      ),
+                      child: ListTile(
+                        leading: ClipRRect(
+                          child: Image.network(
+                            'https://cdn.pixabay.com/photo/2021/06/17/05/14/city-6342765_960_720.jpg',
+                            fit: BoxFit.cover,
+                            width: 100.0,
+                            height: 100.0,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        title: Text(
+                          article.title,
+                        ),
+                        subtitle: Text(article.description),
+                        trailing: Text("${article.price.toString()}€"),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
           }
-          return CircularProgressIndicator();
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         },
-        future: articles,
       ),
     );
   }
